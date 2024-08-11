442ee69232963164ebce278d14ac4945c3a08297
RishankJindal <rishankjindal.regular@gmail.com>
2024-08-11 20:54:01 +0530

    add data_types

---
+++ data_types_and_variables/data_types.py
@@ -0,0 +1,28 @@
+#1 Numeric : int,float,complex
+#2 String
+#Boolean : true,false
+
+# Numeric
+a = 10     #integer
+b = 10.0   #float
+c = 5 + 3j #complex
+
+# String
+c = "Hello"
+d = 'World'
+e = '''This is multiline
+string in python programming.'''
+
+# Boolean
+isContentLoaded = True
+isActive = False
+
+
+# -----------------Type Casting----------------
+
+x = "97" #a
+intx = int(x)
+floatx = float(x)
+charx = chr(intx)
+
+print(charx)
