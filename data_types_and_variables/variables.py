a490522ea9fd9ab3d27d9edf6171851c30f9bd42
RishankJindal <rishankjindal.regular@gmail.com>
2024-08-13 21:21:40 +0530

    add variables

---
+++ data_types_and_variables/variables.py
@@ -0,0 +1,36 @@
+# A name that referes to a memory location where actual value stored.
+
+# 1) Start with letter(a-z,A-Z) or underscore(_).
+# 2) only contain alphnumeric(a-z,A-Z,0-9) and underscore(_).
+# 3) these are case sensitive , var1 , VAR1.
+# 4) reserved words cannot be variable names (if,else,for),
+
+# Camel Case : myVar , myName (variables)
+# Pascal Case: SumTwo (classes)
+# Snake case : my_file_name (file name)
+
+
+# Normal assignment
+
+myVar = "Hello"
+myNum = 12
+
+# Multiple assignment
+
+a,b,c = 1,5,"Python"
+
+
+
+# Swapping variables
+
+a, b =10,20
+print(a)
+print(b)
+
+a,b = b,a
+
+print(a)
+print(b)
+
+
+
