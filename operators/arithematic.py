d8e7c0b128ca9f7bdcbd36118a2d0cf75f1572d0
RishankJindal <rishankjindal.regular@gmail.com>
2024-08-14 12:59:11 +0530

    add atithematic operators

---
+++ operators/arithematic.py
@@ -0,0 +1,20 @@
+# Arithematic Operators
+
+# '+' --> Addition
+# '-' --> Subtraction
+# '*' --> Multiplication (asterisk)
+# '/' --> Division
+# '//' --> Floor Division
+# '%' --> Modulus (Remainder)
+# '**' --> Exponentiation (Power)
+
+num1 = 10
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
