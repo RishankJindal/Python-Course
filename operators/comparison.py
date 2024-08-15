a15e3eebbc9dbab1871697eb380a2963f8c81198
RishankJindal <rishankjindal.regular@gmail.com>
2024-08-15 10:48:37 +0530

    add comparison operators

---
+++ operators/comparison.py
@@ -0,0 +1,16 @@
+# Comparison Operators
+
+# '==' --> Equal to
+# '!=' --> Not equal to
+# '>' --> Greater than
+# '<' --> Less than
+# '>=' --> Greater than or(या) equal to
+# '<=' --> Less than or(या) equal to
+
+num1 = 10
+num2 = 5
+num3 = 5
+
+print(num1 <= num2)
+
+
