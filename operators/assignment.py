a574d16c4041ba744790ca72aea34fa78e3f81fa
RishankJindal <rishankjindal.regular@gmail.com>
2024-08-18 20:54:14 +0530

    add assignment operators

---
+++ operators/assignment.py
@@ -0,0 +1,41 @@
+# Assignment Operators
+
+# '=' --> Equal to (Normal Assignment)
+
+var1 = 12
+var2 ="Rishank Jindal"
+
+# '+=' --> Plus-Equal to (Update by add number)
+
+var1+= 3
+
+
+# '-=' --> Plus-Equal to (Update by subtact number)
+
+var1-= 2
+
+
+# '*=' --> Plus-Equal to (Update by multiply number)
+
+var1*= 2
+
+
+# '/=' --> Plus-Equal to (Update by divide number)
+
+# var1/=2
+
+
+# '//=' --> Plus-Equal to (Update by its floor division with number)
+
+var1//=2
+
+# '%=' --> Plus-Equal to (Update by its modulus with number)
+
+var1 %= 2 #var1 = var1 <operator> 2
+
+
+# '**=' --> Plus-Equal to (Update by its power with number)
+
+var1 = 2
+var1 **= 3
+print(var1)
